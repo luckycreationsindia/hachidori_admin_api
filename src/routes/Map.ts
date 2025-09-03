ebca6043ce8577c11dd39dcfe1bd7074742c8018
Lokesh Jain <luckycreationsindia@gmail.com>
2025-09-03 21:41:00 +0530

    Restrict `getMap` route access to admin users with `authMiddleware`.

--- src/routes/Map.ts
+++ src/routes/Map.ts
@@ -1,8 +1,10 @@
 import {Router} from "express";
 import * as MapController from "../controllers/Map";
+import {authMiddleware} from "../middlewares";
+import {Role} from "../utils/consts";
 
 const router = Router();
 
-router.get('/:id', MapController.getMap);
+router.get('/:id', authMiddleware({role: Role.ADMIN}), MapController.getMap);
 
 export default router;
